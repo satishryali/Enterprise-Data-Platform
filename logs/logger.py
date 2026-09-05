bc9f587ef204ce51b822793bc48805e1dddf333b
satishryali <104883533+satishryali@users.noreply.github.com>
2026-09-05 10:01:08 +0530

    Server (#1)
    * Committing my work on csv working engine
    
    * Server intial Structure completed

---
+++ logs/logger.py
@@ -0,0 +1,5 @@
+import logging
+
+logging.basicConfig(filename = "applog.log", format = "%(asctime)s | %(level)s | %(message)s", level = logging.DEBUG)
+
+logging.info("Satish ryali is a coder and he codes very well")
